40c11fcc65f2eb11b3813880adbedef4573d6779
Ethan Szeto <ethan.szeto@gmail.com>
2025-04-10 18:49:32 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { BlogCard } from "@/components/general/BlogCard";
+import { BlogCard, BlogCardProps } from "@/components/general/BlogCard";
 
 export default async function Home() {
   /**
@@ -8,6 +8,14 @@ export default async function Home() {
    * Question 2:
    * How can I display all the blogs on the home page?
    */
+  const response = await fetch("http://localhost:3001/api/all-blogs", { method: "GET" });
+  const data = await response.json();
 
-  return <></>;
+  return (
+    <>
+      {data.blogs.map((blog: BlogCardProps, key: number) => {
+        return <BlogCard key={key} title={blog.title} content={blog.content} author={blog.author} date={blog.date} />;
+      })}
+    </>
+  );
 }
